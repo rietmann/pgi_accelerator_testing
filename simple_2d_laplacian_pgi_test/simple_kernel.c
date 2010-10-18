648b09ea4160226527f256b03156d6207bef062b
Maximilian Riethmann <rietmann@eiger200.adm.cscs.ch>
2010-10-18 11:09:29 +0200

    saving progress so far before changing doubly nested loop into single loop with more complex index calculation

--- simple_2d_laplacian_pgi_test/simple_kernel.c
+++ simple_2d_laplacian_pgi_test/simple_kernel.c
@@ -2,29 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void laplacian_cpu(float *u_out, float *u, int x_max, int y_max);
+void laplacian_pgi(float *u_out, float *u, int x_max, int y_max);
+
 void laplacian_pgi(float *u_out, float *u, int x_max, int y_max) {
 
   int x,y;
-#pragma acc region copyin(u[0:x_max*y_max-1]) copyout(u_out[0:x_max*y_max-1])
+#pragma acc region copyin(u[0:x_max*y_max]) copy(u_out[0:x_max*y_max])
   {
-#pragma acc for independent
+    /* #pragma acc for independent */
     for (y=1; y<y_max-1; y++) {
-#pragma acc for independent
-      for(x=1;x<x_max-1; x++) {
+      /* #pragma acc for independent */
+      for(x=1; x<x_max-1; x++) {
 
 	int id = x + x_max*y;
 	int idx_m1 = id-1;
 	int idx_p1 = id+1;
 	int idy_m1 = id-x_max;
 	int idy_p1 = id+x_max;
-
-	u_out[id] = -u[id] + 0.25*(u[idx_m1] + u[idx_p1] + u[idy_m1] + u[idy_p1]);
+	/* u_out[id] = -u[id] + 0.25*(u[idx_m1] + u[idx_p1] + u[idy_m1] + u[idy_p1]); */
+	u_out[x+8*y] = id;
       
       }
     
     }
   } 
 }
+
+/* void laplacian_pgi_v2(float *u_out, float *u, int x_max, int y_max) { */
+/*   int x, y; */
+/* #pragma acc region copyin((u[0])[0:(x_max*y_max)-1]) copyout((u_out[0])[0:(x_max*y_max)-1]) */
+/*   { */
+/* #pragma acc for independent */
+/*     for(y=1; y<y_max-1; y++) { */
+/* #pragma acc for independent */
+/*       for(x=1; x<x_max-1; x++) { */
+/* 	u_out[x][y] = u[x][y] + 0.25*(u[x-1][y] + u[x+1][y] + u[x][y-1] + u[x][y+1]); */
+/*       } */
+/*     } */
+/*   } */
+/* } */
 
 void laplacian_cpu(float *u_out, float *u, int x_max, int y_max) {
 
@@ -38,9 +55,10 @@ void laplacian_cpu(float *u_out, float *u, int x_max, int y_max) {
       int idx_m1 = id-1;
       int idx_p1 = id+1;
       int idy_m1 = id-x_max;
-      int idy_p1 = id+x_max;
+      int idy_p1 = id+x_max;	 
 
-      u_out[id] = -u[id] + 0.25*(u[idx_m1] + u[idx_p1] + u[idy_m1] + u[idy_p1]);
+      /* u_out[id] = -u[id] + 0.25*(u[idx_m1] + u[idx_p1] + u[idy_m1] + u[idy_p1]); */
+      u_out[id] = id;
       
     }
     
@@ -48,7 +66,7 @@ void laplacian_cpu(float *u_out, float *u, int x_max, int y_max) {
   
 }
 
-int main() {
+int main(int argc, char** argv) { 
 
   int x,y;
   
@@ -59,12 +77,15 @@ int main() {
   float* u_out;
   float* u_cpu;
   float* u_out_cpu;
-
-  u = (float*)malloc(x_max*y_max*sizeof(float));
-  u_out = (float*)malloc(x_max*y_max*sizeof(float));
-  u_cpu = (float*)malloc(x_max*y_max*sizeof(float));
-  u_out_cpu = (float*)malloc(x_max*y_max*sizeof(float));
-
+  float** u2;
+  float** u_out2;
+  
+  
+  u = (float*)malloc(x_max*(y_max+4)*sizeof(float));
+  u_out = (float*)malloc(x_max*(y_max+4)*sizeof(float));
+  u_cpu = (float*)malloc(x_max*(y_max+4)*sizeof(float));
+  u_out_cpu = (float*)malloc(x_max*(y_max+4)*sizeof(float));  
+  
   for(x=0;x<x_max*y_max;x++) {
     u[x] = 1.1;
     u_out[x] = -42;
@@ -72,20 +93,30 @@ int main() {
     u_out_cpu[x] = -42;
   }
 
-  printf("launching pgi kernel\n");
+  /* printf("trying pgi_v2 w/ multi-dimensional array\n"); */
+  /* laplacian_pgi_v2(u2,u_out2,x_max,y_max); */
+  printf("launching pgi kernel\n");  
   laplacian_pgi(u_out, u, x_max, y_max);
   printf("launching simple cpu kernel\n");
   laplacian_cpu(u_out_cpu,u_cpu, x_max, y_max);
-  
-  double error=0;
-  int error_count=0;
-  for(x=0;x<x_max*y_max;x++) {
-    error = fabs(u_out[x] - u_out_cpu[x]);
-    if(error > 0.001) {      
-      error_count++;
-      printf("Error %d@u_out[%d]: |%f-%f|=%f\n",error_count, x, u_out[x],u_out_cpu[x],error);
-      if(error_count > 30) { exit(1); }
-    }
+
+
+  /* double error=0; */
+  /* int error_count=0; */
+  /* for(x=0;x<x_max*y_max;x++) { */
+  /*   error = fabs(u_out[x] - u_out_cpu[x]); */
+  /*   if(error > 0.001) {       */
+  /*     error_count++; */
+  /*     printf("Error %d@u_out[%d]: |%f-%f|=%f\n",error_count, x, u_out[x],u_out_cpu[x],error); */
+  /*     if(error_count > 30) { break;} */
+  /*   } */
+  /* } */
+
+  printf("the first few cpu & gpu results\n");
+  for(x=0;x<64;x++) {
+    int x1 = floor(x/x_max);
+    int x2 = x%x_max;
+    printf("u_out_pgi[%d]=%2.2f, u_out_cpu[%d]=%2.2f\n",x,u_out[x],x,u_out_cpu[x]);
   }
   
 }
